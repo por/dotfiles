9727e5a2fa65c55da5f9aa8f013bf17b726afca2
por <hello@timbormans.com>
2016-10-08 19:01:00 +0200

    Remove broken keyrepeat in MacOS Sierra

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -51,9 +51,6 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
-
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
