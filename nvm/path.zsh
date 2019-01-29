db4afc076e321825300eed21c0a57fcdd237116e
por <hello@timbormans.com>
2019-01-29 11:12:18 +0100

    Speed up bash load time by lazy loading nvm

--- nvm/path.zsh
+++ nvm/path.zsh
@@ -1,2 +1,8 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm_load () {
+  . $NVM_DIR/nvm.sh
+  . $NVM_DIR/bash_completion
+}
+alias node='unalias nvm; unalias node; unalias npm; nvm_load; node $@'
+alias npm='unalias nvm; unalias node; unalias npm; nvm_load; npm $@'
+alias nvm='unalias nvm; unalias node; unalias npm; nvm_load; nvm $@'
