e0e51d4bb9b6d1354e9877da1585ea22a6360a1b
Tim Bormans <hello@timbormans.com>
2026-01-04 16:37:25 +0100

    Consolidate shell options in config.zsh
    Moved unsetopt CORRECT from zshrc.symlink to config.zsh to keep all
    shell option configuration in one place. Improves organization and
    eliminates contradiction.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -18,18 +18,14 @@ setopt HIST_VERIFY
 setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
-setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
-
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
-
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+setopt complete_aliases # expand aliases before completion (e.g. git comm-[tab])
+unsetopt CORRECT # disable autocorrect
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
