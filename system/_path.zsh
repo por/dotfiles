a26289d1e371cd5265078ee7757116bfdaf987fc
Tim Bormans <hello@timbormans.com>
2026-01-04 15:28:21 +0100

    Fix PATH security vulnerability and duplicate $PATH
    Removed ./bin from PATH to prevent command hijacking vulnerability.
    Removed duplicate $PATH entry that caused path ordering confusion.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$HOME/.local/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$HOME/.local/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
